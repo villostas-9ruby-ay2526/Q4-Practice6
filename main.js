b2982907f19ff2396a316a419b613b5a5ad598ab
VILLOSTAS DV <villostas.dv@obmontessori.edu.ph>
2026-04-24 12:36:14 +0800

    ll

---
+++ main.js
@@ -0,0 +1,7 @@
+function enumerateAnimals(){
+    var animals = ["Sea Lion, Cockatoo, Lizard, Wooly Mammoth"]
+
+    for (var i = 0; i < animals.length; i++ ) {
+        window.alert("This zoo contains a " +animals[i] + ".");
+    }
+}
